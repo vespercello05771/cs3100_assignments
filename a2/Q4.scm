3c11700869e565fb9910aa96573818c2a29ec346
vespercello05771 <87663330+vespercello05771@users.noreply.github.com>
2022-09-11 10:17:10 +0530

    Delete a2 directory

--- a2/Q4.scm
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(define make-iterator (lambda (l) (letrec ((next (lambda (n)
-(delay )))))
-(cdr l)))
-
-(define next(lambda (l) (force (car l))))
-
-
--- a2/Q6.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(define mix (lambda (l1 l2) (map cons l1 l2)))
-(mix '(1 2 3) '(alpha beta gamma))
-
-(define unmix (lambda (l) (cons (map car l) (list (map cadr l)))))
-(unmix '((1 alpha) (2 beta) (3 gamma)))
